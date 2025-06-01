f97d113905bb1c32ece6ad432601b4abe30515b8
Adilson Rocha <adilsonrocha40@gmail.com>
2025-05-31 22:32:02 -0300

    atualizações código route, repository e model do módulo perfil

--- backend/src/perfis/model.js
+++ backend/src/perfis/model.js
@@ -1,13 +1,25 @@
 export class Perfil {
-  constructor({ id, nome, descricao }) {
-    this.id = id;
-    this.nome = nome;
-    this.descricao=descricao;
-  }
+    constructor({ id, id_usuario, nome_completo, sexo, data_nascimento, endereco, numero,
+        complemento, bairro, cidade, cep, pais,
+    }) {
+        this.id = id;
+        this.id_usuario = id_usuario;
+        this.nome_completo = nome_completo;
+        this.sexo = sexo;
+        this.data_nascimento = data_nascimento;
+        this.endereco = endereco;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.cep = cep;
+        this.pais = pais;
 
-  static validate(perfil) {
-    if (!perfil.nome || !perfil.descricao) {
-      throw new Error("Nome e descricao são obrigatórios");
     }
-  }
+
+    static validate(perfil) {
+        if (!perfil.nome_completo || !perfil.data_nascimento) {
+            throw new Error("Nome completo e data de nascimento são obrigatórios!");
+        }
+    }
 }
--- backend/src/perfis/repository.js
+++ backend/src/perfis/repository.js
@@ -1,84 +1,127 @@
 import db from "../../db.js";
 
-const tabela = "perfils";
+
+const tabela = "perfil";
 
 export const create = async (dados) => {
-  const dadosMapeados = {
-    nome: dados.nome,
-    descricao: dados.descricao,
+    const dadosMapeados = {
+        id_usuario: dados.id_usuario,
+        created_at: dados.created_at,
+        nome_completo: dados.nome_completo,
+        sexo: dados.sexo,
+        data_nascimento: dados.data_nascimento,
+        endereco: dados.endereco,
+        numero: dados.numero,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.cidade,
+        cep: dados.cep,
+        pais: dados.pais
     };
-  const result = await db(tabela).insert(dadosMapeados).returning("*");
+    const result = await db(tabela).insert(dadosMapeados).returning("*");
 
-  return result;
+    return result;
 };
 
 export const findAll = async () => {
-  const perfil = await db(tabela).select("*");
+    const perfil = await db(tabela).select("*");
 
-  return perfil.map((perfil) => ({
-    id: perfil.id,
-    nome: perfil.nome,
-    descricao: perfil.descricao,
-  }));
+    return perfil.map((perfil) => ({
+        id: perfil.id,
+        id_usuario: perfil.id_usuario,
+        created_at: perfil.created_at,
+        nome_completo: perfil.nome_completo,
+        sexo: perfil.sexo,
+        data_nascimento: perfil.data_nascimento,
+        endereco: perfil.endereco,
+        numero: perfil.numero,
+        complemento: perfil.complemento,
+        bairro: perfil.bairro,
+        cidade: perfil.cidade,
+        cep: perfil.cep,
+        pais: perfil.pais
+
+
+    }));
 };
 
 export const findOne = async (id) => {
-  const perfil = await db(tabela).where({ id }).first();
+    const perfil = await db(tabela).where({ id }).first();
 
-  if (perfil) {
-    return perfil;
-  }
-  return null;
+    if (perfil) {
+        return perfil;
+    }
+    return null;
 };
 
 export const update = async (id, dados) => {
-  const dadosMapeados = {
-    id: dados.id,
-    nome: dados.nome,
-    descricao: dados.descricao,
-  };
+    const dadosMapeados = {
+        id: dados.id,
+        id_usuario: dados.id_usuario,
+        created_at: dados.created_at,
+        nome_completo: dados.nome_completo,
+        sexo: dados.sexo,
+        data_nascimento: dados.data_nascimento,
+        endereco: dados.endereco,
+        numero: dados.numero,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.cidade,
+        cep: dados.cep,
+        pais: dados.pais,
+    };
 
-  const result = await db(tabela)
-    .where({ id })
-    .update(dadosMapeados)
-    .returning("*");
+    const result = await db(tabela)
+        .where({ id })
+        .update(dadosMapeados)
+        .returning("*");
 
-  if (!result || result.length === 0) {
-    throw new Error("Nenhum registro foi atualizado.");
-  }
-  return result[0];
+    if (!result || result.length === 0) {
+        throw new Error("Nenhum registro foi atualizado.");
+    }
+    return result[0];
 };
 
 export const patch = async (id, dados) => {
-  const dadosMapeados = {
-    nome: dados.nome,
-    descricao: dados.descricao,
-  };
+    const dadosMapeados = {
+        id_usuario: dados.id_usuario,
+        created_at: dados.created_at,
+        nome_completo: dados.nome_completo,
+        sexo: dados.sexo,
+        data_nascimento: dados.data_nascimento,
+        endereco: dados.endereco,
+        numero: dados.numero,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.cidade,
+        cep: dados.cep,
+        pais: dados.pais,
+    };
 
-  const result = await db(tabela)
-    .where({ id })
-    .update(dadosMapeados)
-    .returning("*");
+    const result = await db(tabela)
+        .where({ id })
+        .update(dadosMapeados)
+        .returning("*");
 
-  if (!result || result.length === 0) {
-    throw new Error("Nenhum registro foi atualizado.");
-  }
-  return result[0];
+    if (!result || result.length === 0) {
+        throw new Error("Nenhum registro foi atualizado.");
+    }
+    return result[0];
 };
 
 export const remove = async (id) => {
-  const result = await db(tabela).where({ id }).delete().returning("*");
+    const result = await db(tabela).where({ id }).delete().returning("*");
 
-  if (!result || result.length === 0) {
-    throw new Error("Nenhum registro foi removido.");
-  }
-  return result[0];
+    if (!result || result.length === 0) {
+        throw new Error("Nenhum registro foi removido.");
+    }
+    return result[0];
 };
 
 export const repository_perfis = {
-  create,
-  findAll,
-  findOne,
-  update,
-  remove,
+    create,
+    findAll,
+    findOne,
+    update,
+    remove,
 };
