d0113387f7ed65eda8808780649437d413163661
Philippe Henry <philippe.henry@owse.com.br>
2025-06-03 10:55:09 -0300

    Commit dos Módulos

--- backend/src/modulos/route.js
+++ backend/src/modulos/route.js
@@ -10,11 +10,17 @@ import {
 
 const router = express.Router();
 
+/*Listar Módulo*/
 router.get("/", getAllModulos);
+/*Visualizar Módulo*/
 router.get("/:id", getModulos);
+/*Criar Módulo*/
 router.post("/", createModulos);
+/*Atualizar Módulo*/
 router.put("/:id", updateModulos);
+/*Editar Módulo*/
 router.patch("/:id", patchModulos);
+/*Deletar Módulo*/
 router.delete("/:id", deleteModulos);
-
+/*Criar Módulo*/
 export default router;
