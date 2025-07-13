c9678438ef23b35afbcf5b13c46972c40ef5cf76
caina <cainazuma@yahoo.com>
2025-07-13 06:26:40 -0300

    fix: vercel swagger config

--- backend/swagger.js
+++ backend/swagger.js
@@ -1,7 +1,14 @@
 // @ts-nocheck
 import swaggerUi from "swagger-ui-express";
 import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-const swaggerDocument = JSON.parse(readFileSync("./swagger.json", "utf8"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const swaggerDocument = JSON.parse(
+  readFileSync(join(__dirname, "swagger.json"), "utf8")
+);
 
 export { swaggerDocument, swaggerUi };
